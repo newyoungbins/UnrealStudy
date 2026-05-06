00d978ca160df7e38d0559a1ab4854847856bbfb
정영빈 <dudqlsjyb@naver.com>
2026-05-07 07:10:39 +0900

    Add ObjectReflection

---
+++ UnrealStudy01/ObjectReflection/Source/ObjectReflection/MyGameInstance.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstance.h"
+
+UMyGameInstance::UMyGameInstance()
+{
+	SchoolName = TEXT("기본학교");
+}
+
+void UMyGameInstance::Init()
+{
+	Super::Init();
+
+	UE_LOG(LogTemp, Log, TEXT("========================="));
+	UClass* ClassRuntime = GetClass();
+	UClass* ClassCompile = UMyGameInstance::StaticClass();
+	//check(ClassRuntime == ClassCompile);     //==를 !=로 해서 실행하면 에디터가 꺼진다.
+	//ensure(ClassRuntime != ClassCompile);		// 이것은 꺼지고 난 후 다시 키기 귀찮을때 사용한다.
+	//ensureMsgf(ClassRuntime != ClassCompile, TEXT("일부로 에러를 발생시킨 코드")); 
+
+	UE_LOG(LogTemp, Log, TEXT("학교를 담당하는 클래스 이름 : %s"), *ClassRuntime->GetName());
+
+	SchoolName = TEXT("청강문화산업대학교");
+
+	UE_LOG(LogTemp, Log, TEXT("학교 이름 : %s"), *SchoolName);
+	UE_LOG(LogTemp, Log, TEXT("학교 이름 기본값 : %s"), *GetClass()->GetDefaultObject<UMyGameInstance>()->SchoolName);
+
+	UE_LOG(LogTemp, Log, TEXT("========================="));
+}
